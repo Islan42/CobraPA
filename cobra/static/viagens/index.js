88797c1296a83b3fce6cf4d44092c2b18c8594f2
Islan <islan12victhor@gmail.com>
2024-06-15 20:39:38 -0300

    adicionei Carrossel utilizando jQuery e Tailwind *-*. É preciso, porém, mudar o jQuery de CDN para webpacked

--- cobra/static/viagens/index.js
+++ cobra/static/viagens/index.js
@@ -1,3 +1,29 @@
 jQuery( function(){
-    
+    /*$.ajax({
+        url: '/api/',
+        method: 'GET',
+        dataType: 'json'
+    }).done(function(data){
+        $('#carrossel-home').empty();
+
+        $.each(data, function(i, cidade){
+            $('#carrossel-home').append(
+                `<li>${cidade.nome} - <a href="/cidade/${cidade.id}">Comprar</a></li>`
+            )
+        });
+    });
+    */
+
+    const carrosselLista = $('#carrossel-home-lista li').toArray()
+    const carrosselControle = $('#carrossel-home-controle span').toArray()
+
+    $.each(carrosselControle, function(i, controle){
+        $(controle).on('click', function(event){
+            $.each(carrosselLista, function(i, item){
+                $(item).addClass('hidden')
+            })
+
+            $(carrosselLista[i]).removeClass('hidden')
+        })
+    });
 })
